192c247332343b583930b822594564587c2de5af
Travo100 <travo100@gmail.com>
2018-08-14 21:45:58 -0700

    Added Subclasses Exercise

---
+++ src/_11_Stu_Subclasses/Solved/Square.java
@@ -0,0 +1,15 @@
+package _11_Stu_Subclasses.Solved;
+
+import java.lang.Math;
+
+class Square extends Shape {
+  
+  Square (int sideLength) {
+    super(4, sideLength);
+  }
+
+  @Override double area () {
+    return Math.pow(this.mSideLength, 2);
+  }
+
+}
---
+++ src/_11_Stu_Subclasses/Solved/Triangle.java
@@ -0,0 +1,19 @@
+package _11_Stu_Subclasses.Solved;
+
+import java.lang.Math;
+
+class Triangle extends Shape {
+  
+  Triangle (int sideLength) {
+    super(3, sideLength);
+  }
+
+  @Override double area () {
+    return 0.5 * this.mSideLength * this.getHeight();
+  }
+
+  private double getHeight () {
+    return this.mSideLength * Math.sin(Math.toRadians(60));
+  }
+
+}
