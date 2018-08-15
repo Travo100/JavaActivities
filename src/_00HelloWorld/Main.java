c2e0928100cbdf5b3a5b67878449063b988b91b9
Travo100 <travo100@gmail.com>
2018-08-14 20:09:17 -0700

    Init commit

---
+++ src/_00HelloWorld/Main.java
@@ -0,0 +1,9 @@
+package _00HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Hello World with Java! */
+        System.out.println("Hello World!");
+    }
+}
