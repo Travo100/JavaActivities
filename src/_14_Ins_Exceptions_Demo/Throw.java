63589b842b2fb4ee40b38524bf310ceffeb7e104
Travo100 <travo100@gmail.com>
2018-08-15 18:03:49 -0700

    Added in a _14_Ins_Exceptions_Demo

--- src/_14_Ins_Exceptions_Demo/Throw.java
+++ src/_14_Ins_Exceptions_Demo/Throw.java
@@ -2,6 +2,30 @@ package _14_Ins_Exceptions_Demo;
 
 public class Throw {
     public static void main (String[] args) {
-        System.out.println("Hello");
+
+        printSeparator();
+
+        try {
+            throw new Exception("Oh no! A disaster!");
+        } catch (Exception e) {
+            System.out.println("Exception caught. Continuing execution...");
+        }
+
+//        try {
+//            throw new Exception("Oh no! A disaster!");
+//        } catch (Exception e) {
+//            System.out.println("Exception caught. Continuing execution...");
+//        } catch (ClassCastException e) {
+//            System.out.println("Actually, this is impossible here...");
+//        }
+
+        System.out.println("This will run, now that we have a catch block.");
+
+        printSeparator();
+
+    }
+
+    private static void printSeparator () {
+        System.out.println(new String(new char[6]).replace("\0", "--"));
     }
 }
