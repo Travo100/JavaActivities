819f3ac7473d820b83e1d8eb222d2b347c495f9e
Travo100 <travo100@gmail.com>
2018-08-15 21:11:14 -0700

    Added in a _16_Stu_Varargs

---
+++ src/_16_Stu_Varargs/Unsolved/Streams.java
@@ -0,0 +1,13 @@
+package _16_Stu_Varargs.Unsolved;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.stream;
+
+class Streams {
+
+  public static void main (String[] args) {
+    // Put your tests here.
+  }
+
+}
